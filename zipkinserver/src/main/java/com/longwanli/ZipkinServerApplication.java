abbdb6698518d560a09c945f0b300bdca3f9b635
EF <1245146940@qq.com>
2020-11-01 17:50:11 +0800

    springcloud学习代码,加载远端配置文件

---
+++ zipkinserver/src/main/java/com/longwanli/ZipkinServerApplication.java
@@ -0,0 +1,14 @@
+package com.longwanli;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer //声明启动Zipkin Server
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class,args);
+    }
+}
